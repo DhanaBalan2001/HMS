b05124e749b8d0abfb867dd40ecf4370e42df917
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2026-01-13 15:58:29 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -47,9 +47,7 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? ['https://healthapp-hemapathi.netlify.app'] 
-    : ['http://localhost:5173'],
+  origin:['https://healthapp-hemapathi.netlify.app','http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
